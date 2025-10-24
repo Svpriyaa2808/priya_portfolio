00896d4b5a843ccb0b0b3d9836e90315b8400312
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-10-24 13:18:04 +0200

    fixing alignment

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,11 @@ export default function Home() {
     <div className="text-white m-4 grow">
       <div className="flex flex-col md:flex-row mt-4">
         <div className="basis-[40%] ">
-          <p className="text-xl m-4 lg:m-8 px-8">Hey, I'm <span className="text-[#B4ADA3] font-bold">Priya</span> 👋🏻</p>
-          <h1 className="text-3xl md:text-5xl font-bold text-center m-2 mt-6">Frontend Developer</h1>
-          <p className=" md:text-[16px] lg:text-xl m-8 px-0 md:px-8">I'm a Front-End Development student . I'm reaching out to see if your team might be open to taking on a front-end development intern</p>
+          <p className="text-[18px] md:text-xl 2xl:text-2xl lg:text-2xl m-4 lg:m-8 px-8">Hey, I'm <span className="text-[#B4ADA3] font-bold">Priya</span> 👋🏻</p>
+          <h1 className="text-3xl md:text-5xl 2xl:text-6xl font-bold text-center m-2 mt-6">Frontend Developer</h1>
+          <p className=" md:text-[16px] lg:text-xl 2xl:text-2xl m-8 px-0 md:px-8">I'm a Front-End Development student . I'm reaching out to see if your team might be open to taking on a front-end development intern</p>
           
-          <div className="flex flex-col sm:flex-row lg:flex-row md:flex-col m-8 px-8 pt-4 gap-4 items-center justify-center lg:justify-start ">
+          <div className="flex flex-col sm:flex-row lg:flex-row md:flex-col m-8 px-8 pt-4 gap-4 items-center justify-center lg:justify-start 2xl:justify-center ">
             <Link href='/project' className="button header-hover max-w-[150px]">Browse Projects</Link>
             <Link href="https://drive.google.com/file/d/1R-N6YcgSeO56GPtEA-_kcGU1ZIZdj7Nt/view?usp=sharing" target="_blank" className="button header-hover max-w-[150px]">
                 My Resume
--- src/app/project/page.js
+++ src/app/project/page.js
@@ -9,11 +9,11 @@ const unicalFont = Uncial_Antiqua({subsets : ["latin"],weight:"400"})
 const ProjectPage = () => {
     return (
         <div className="grow text-center">
-            <div className="text-white m-4 mt-8 text-[18px] text-justify">
-                <p className="indent-9 m-4 mx-8">As a frontend development student, I've worked on a variety of projects as part of my coursework, focusing on building responsive, accessible, and user-friendly interfaces. 
+            <div className="text-white m-4 mx-12 mt-8 max-w-8xl text-[18px] md:text-[20px] text-justify">
+                <p className="indent-14 m-4 mx-8">As a frontend development student, I've worked on a variety of projects as part of my coursework, focusing on building responsive, accessible, and user-friendly interfaces. 
                     Each project provided an opportunity to apply and strengthen my skills in HTML, CSS, JavaScript, and frontend frameworks like React. These projects showcase my ability to translate design 
                     concepts into functional web applications, follow best practices in frontend development, and continuously learn through hands-on experience.</p>
-                <p  className="indent-9 m-4 mx-8">Below are some of the key projects I completed during my course. They reflect my growth, problem-solving skills, and attention to detail as an aspiring frontend developer.</p>
+                <p  className="indent-14 m-4 mx-8">Below are some of the key projects I completed during my course. They reflect my growth, problem-solving skills, and attention to detail as an aspiring frontend developer.</p>
             </div> 
             <div className="flex flex-col sm:flex-row m-4 sm:flex-wrap  justify-around">
             {projectArray.map((item,index)=>
